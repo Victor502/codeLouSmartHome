d12cbe604a3829d22f70e877e7c225163b1ef156
Victor Horton <victorahorton@yahoo.com>
2017-08-12 13:15:47 -0400

    added ansmition

--- foundation-6/js/app.js
+++ foundation-6/js/app.js
@@ -36,6 +36,14 @@ $overlay.click(function (){
 });
 
 
+$('.animsition').animsition({
+  inClass: 'fade-in-right-lg',
+  outClass: 'fade-out-right-lg',
+  linkElement: 'header a',
+  inDuration: 1000,
+  outDuration: 500
+});
+
 
 
 
